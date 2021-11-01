299b7f1684b44d25ddf76fc8e68ca752bd285787
SHISHIR GHIMIRE <sghimi01@gmail.com>
2021-11-01 20:45:33 +1100

    control-flow

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-function scuberGreetingForFeet(){
+
+
+function scuberGreetingForFeet(distance,){
+  
+  let charges; 
+  if (distance <= 400) {
+    charges = 'This one is on me!';
+  } else if ((distance >= 2000) && (distance <2499)) {
+    charges = 'I will gladly take your thirty bucks.';
+  } else if (distance >= 2500)  {
+    charges = 'No can do.';
+  }
+  
+  return `${charges}`;
+}
+console.log(scuberGreetingForFeet("2501"));
+
+
+
+function ternaryCheckCity(city){
   // Write your code here!
+
+return (city === "NYC" ? 'Ok, sounds good.' : 'No go.');
+
 }
 
-function ternaryCheckCity(){
+console.log(ternaryCheckCity("Pittsburge"));
+
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let greet;
+  switch (tip) {
+    case "generous":
+      greet = "Thank you so much.";
+      break;
+    case "not as generous":
+      greet = "Thank you.";
+      break;
+    default: 
+      greet = "Bye.";
+  }
+  
+  return `${greet}` ;
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+console.log(switchOnCharmFromTip("generous"));
